0ba63e90eb6970766f53be8d7c8eab737e972693
Victor Sergienko <singalen@gmail.com>
2015-05-09 14:19:17 +0300

    AndrewA's patch dated 2015-03-10

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from bottle import route, run, static_file, template
 
@@ -9,10 +10,21 @@ def server_static():
 def server_static():
     return template('./html/leaflet_control.html')
 
+@route('/ne/')
+def server_static():
+    return template('./html/ne_index.html')
 
 @route('/slider/')
 def server_static():
     return template('./html/slider.html')
+
+@route('/slider2/')
+def server_static():
+    return template('./html/slider2.html')
+
+@route('/json/test2')
+def server_static():
+    return static_file('data2.json', root = './')
 
 @route('/leaflet/js')
 def leaflet_js():
@@ -22,10 +34,16 @@ def leaflet_js():
 def leaflet_css():
     return static_file('leaflet.css', root = './html/')
 
+@route('/test2/')
+def static():
+    return static_file('data.json', root = './')
+
 @route('/json')
 def json():
     return static_file('data.json', root = './')
 
-
+@route('/json/ne/<file:path>')
+def return_json(file):
+    return static_file(file, root = './ready_json/NE/')
 
 run(host='localhost', port=8811, debug=True)
